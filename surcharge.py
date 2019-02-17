9dc5bbc4d41efd57a15c41af873e8c309a37d8fd
AXEL DUJARDIN <axel.dujardin@atos.net>
2019-02-17 02:56:13 +0100

    la surcharge applique: second part

--- surcharge.py
+++ surcharge.py
@@ -2,23 +2,22 @@ class Employe():
     def __init__(self):
         self.salaireHoraire = 20
 
-    # def printSalaire(self):
-    #   print('Le salaire de base et de {0}$/h'.format(self.salaireHoraire))
+    def printSalaire(self):
+        print('Le salaire de base est de {0}$/h'.format(self.salaireHoraire))
 
 class Ingenieur(Employe):
     def __init__(self,prenom):
         Employe.__init__(self)
         self.prenom = prenom
+        self.salaireHoraire = 25
 
     def redefinirSalaire(self, nouveauSalaire):
         self.salaireHoraire = nouveauSalaire
 
-    # def printSalaire(self)
-    #   print('Le salaire de {0} est de {1}$/h'.format(self.prenom,self.salaireHoraire))
+    def printSalaire(self):
+        print('Le salaire d\'un ingenieur est de {1}$/h'.format(self.prenom,self.salaireHoraire))
 
 julie = Ingenieur('Julie')
-print(julie.salaireHoraire)
-julie.redefinirSalaire(25)
-print(julie.salaireHoraire)
-tom = Ingenieur('Tom')
-print(tom.salaireHoraire)
+julie.printSalaire()
+tom = Employe()
+tom.printSalaire()
