56aff460a6e3491614d9b815a4c7790f4f9b0055
AXEL DUJARDIN <axel.dujardin@atos.net>
2019-02-17 03:54:10 +0100

    finally execute what i want <3

---
+++ finally.py
@@ -0,0 +1,20 @@
+numerateur = 10
+# denominateur = 0
+# denominateur = 'toto'
+
+resultat = 0
+
+try:
+    resultat = numerateur / denominateur
+
+except ZeroDivisionError:
+    print('Division par zero impossible')
+except TypeError:
+    print('Une des deux variable n\'est pas un nombre')
+except NameError as erreur:
+    if 'numerateur' in str(erreur): # utilisation de str(message) pour python3 au lieu de erreur.message de python2
+        print('Une variable numerateur n\'a pas ete declaree')
+    elif 'denominateur' in str(erreur):
+        print('La variable denominateur n\'a pas ete declarer')
+finally:
+    print(resultat) # Le finally s'executeras quoi qu'il arrive, il faut donc utiliser une valeur par defaut.
