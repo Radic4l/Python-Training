c02c9030a8a24824f8d0ef2866dd392c88460848
AXEL DUJARDIN <axel.dujardin@atos.net>
2019-02-17 03:25:35 +0100

    tryExcept, echapper une erreur

---
+++ tryExcept.py
@@ -0,0 +1,17 @@
+numerateur = 10
+denominateur = 8
+
+try:
+    resultat = numerateur / denominateur
+    print(resultat)
+except:
+    print('Il y a un probleme avec cette operation.')
+
+numerateur2 = 10
+denominateur2 = 0
+
+try:
+    resultat2 = numerateur2 / denominateur2
+    print(resultat2)
+except:
+    print('Il y a un probleme avec cette operation.')
