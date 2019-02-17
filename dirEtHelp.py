992a9848e09815a9418f39b2086c597497c955dc
AXEL DUJARDIN <axel.dujardin@atos.net>
2019-02-17 04:21:02 +0100

    fonction utile pour decouvrir les modules

---
+++ dirEtHelp.py
@@ -0,0 +1,8 @@
+import os
+
+# print(dir(os)) # permet d'afficher toute les fonctions de os
+print(help(os)) # affiche l'aide global du module
+#print(help(os.mkdir)) # affiche uniquement l'aide de la fonction specifier du module
+#print(os.__file__)
+
+# une bonne methode afin de vite decouvrir un module et le parcourir rapidement
