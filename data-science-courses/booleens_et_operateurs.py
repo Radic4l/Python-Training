65da7ba3cc2573038c2f859ea27a03c7bba0efdc
AXEL DUJARDIN <axel.dujardin@atos.net>
2019-02-24 00:06:59 +0100

    basic conditions

---
+++ data-science-courses/booleens_et_operateurs.py
@@ -0,0 +1,22 @@
+# == retourne vrai si les variable sont équivalente
+print(2 == 2)
+# ou faux si elle ne le sont pas
+print(2 == 3)
+
+# != retourne vrais si la valeur des variables sont differentes
+print(2 != 3)
+# ou faut si elle sont equivalente
+print(2 != 2)
+
+# > / >= retourne vrais si la premiere variable et plus grande que la deuxieme / plus grande ou egale a la deuxieme variable
+print(2>1)
+print(2>=2)
+# ou faux si inversement
+print(2>2)
+print(2>=3)
+# Idem pour < / <=
+
+# fonctionnent egaleme,nt avec les liste
+print(["chien","chat"] == ["chien","chat"])
+print(5.0 == 5.0)
+
