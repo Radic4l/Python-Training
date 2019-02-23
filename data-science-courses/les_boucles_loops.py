905f789fb36dae22d66e21614e9246141ff8c5cc
AXEL DUJARDIN <axel.dujardin@atos.net>
2019-02-23 20:22:58 +0100

    ajout loop & separation

---
+++ data-science-courses/les_boucles_loops.py
@@ -0,0 +1,5 @@
+cities = ["Rome","Paris","Madrid"]
+
+for city in cities: # city = variable iteratrive
+    print(city)
+
--- data-science-courses/separation_des_element.py
+++ data-science-courses/separation_des_element.py
@@ -1,2 +1,4 @@
-data = open("sources/departements-fr.csv")
-print(data)
+name = "Tom,Bob,Jack,John"
+split_name = name.split(",")
+print(split_name) # split
+print(name) # no split
