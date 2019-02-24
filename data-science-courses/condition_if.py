ab661d36653ae57e2e3c68a3a7706b4a8672171f
AXEL DUJARDIN <axel.dujardin@atos.net>
2019-02-24 01:32:56 +0100

    quelque façon d'utiliser la condition if

---
+++ data-science-courses/condition_if.py
@@ -0,0 +1,17 @@
+# la condition if
+
+# une premiere façon de faire
+age = 55
+greater = (age > 35)
+
+if greater:
+    print(age)
+
+# une deuxieme façon de faire
+if age > 35:
+    print(age)
+
+value = 1000
+if value > 100:
+    if value > 500:
+        print("Ce nombre est énorme!")
